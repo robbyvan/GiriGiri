311cc99fbd4bd73ecb7388965b29e7f382702dc1
Robby <fzpinus@gmail.com>
2018-08-19 17:18:37 +0800

    feat: fonts

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -34,7 +34,11 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      '@': resolve('src'),
+      'src': resolve('src'),
+      'common': resolve('src/common'),
+      'components': resolve('src/components'),
+      'api': resolve('src/api'),
+      'base': resolve('src/base'),
     }
   },
   module: {
