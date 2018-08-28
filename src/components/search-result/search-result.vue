d2c37a6010a82c475dae849ea8b05e4931c393f4
Robby <fzpinus@gmail.com>
2018-08-28 19:46:59 +0800

    feat: order change

--- src/components/search-result/search-result.vue
+++ src/components/search-result/search-result.vue
@@ -10,17 +10,36 @@
         {{ _categoryText(index) }}
       </button>
     </div>
+    <div class="search-content-wrapper">
+      <div class="total-wrapper">
+        <div class="order-box">
+          <button
+            v-for="(item, index) in orders"
+            :key="item"
+            :class="{'active-order': currentOrderIndex === index }"
+            @click="selectOrder(index)"
+          >
+            {{ item }}
+          </button>
+        </div>
+      </div>
+      <div class="bangumi-wrapper"></div>
+      <div class="upuser-wrapper"></div>
+      <div class="pgc-wrapper"></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    categories: { type: Array, default: () => ['综合', '番剧', 'UP主', '影视'] }
+    categories: { type: Array, default: () => ['综合', '番剧', 'UP主', '影视'] },
+    orders: { type: Array, default: () => ['默认排序', '播放多', '新发布', '弹幕多'] }
   },
   data() {
     return {
       currentCategoryIndex: 0,
+      currentOrderIndex: 0,
       bangumis: [],
       upusers: [],
       pgcs: [],
@@ -42,11 +61,19 @@ export default {
           return '综合';
       }
     },
+    // 切换大类
     selectCategory(index) {
       if (index === this.currentCategoryIndex) {
         return;
       }
       this.currentCategoryIndex = index;
+    },
+    // 切换综合页排序规则
+    selectOrder(index) {
+      if (index === this.currentOrderIndex) {
+        return;
+      }
+      this.currentOrderIndex = index;
     }
   }
 };
@@ -66,9 +93,13 @@ export default {
 }
 
 .category {
-  display: flex;
+  position: fixed;
+  z-index: 3;
+  height: 2rem;
+  background-color: $color-background;
   width: 100%;
-  padding: 0.3rem 1rem;
+  padding: 0.2rem 1rem;
+  display: flex;
   justify-content: space-between;
   align-items: center;
   button {
@@ -79,6 +110,37 @@ export default {
     &.active-category {
       border-bottom: 0.1rem solid $color-theme;
       color: $color-theme;
+    }
+  }
+}
+
+.search-content-wrapper {
+  position: relative;
+  padding-top: 2rem;
+  width: 100%;
+  height: 100%;
+  // background-color: lavender;
+}
+
+.total-wrapper {
+  width: 100%;
+  height: 100%;
+  // background-color: lightcyan;
+  .order-box {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 1rem;
+    height: 1.8rem;
+    background-color: $color-background-l;
+    button {
+      background-color: transparent;
+      font-size: $font-size-small;
+      color: $color-text-gray;
+      padding: 0.2rem 0;
+      &.active-order {
+        color: $color-theme;
+      }
     }
   }
 }
